14df8b5714d92e1ba1cde1f57d02c1725e34003d
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-01-31 11:33:18 +0100

    i18n set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get 'equipe',       to: 'pages#equipe'
-  get 'don',     to: 'pages#don'
-  get 'sabibaba',     to: 'pages#sabibaba'
+  scope '(:locale)', locale: /en/ do
+    root to: 'pages#home'
+    get 'equipe',       to: 'pages#equipe'
+    get 'don',     to: 'pages#don'
+    get 'sabibaba',     to: 'pages#sabibaba'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
