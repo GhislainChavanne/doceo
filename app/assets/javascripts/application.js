9bd54b547fe2cea8a6c4d4223dd9bcd20183638c
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-02-02 19:00:04 +0100

    better css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,18 +3,4 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(window).scroll(
-    {
-        previousTop: 0
-    },
-    function () {
-    var currentTop = $(window).scrollTop();
-    if (currentTop < this.previousTop) {
-        $(".sidebar em").text("Up"); /* optional for demo */
-        $(".navbar-wagon").show();
-    } else {
-        $(".sidebar em").text("Down");
-        $(".navbar-wagon").hide();
-    }
-    this.previousTop = currentTop;
-});
+
